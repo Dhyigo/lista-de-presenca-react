accecfb692c326d6e58026e5e30fdbf078825ee1
= <aldhiego666@gmail.com>
2023-04-24 12:51:27 -0300

    Lista de presença feita em react e ts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import { Home } from './pages/Home'
 import './style/global.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-
-document.getElementById('root'))
+)
